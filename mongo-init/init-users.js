481429bc60924f1a3141ecacd1575399f129a4a3
Benoît Dourdet <benoit.dourdet@gmail.com>
2025-12-31 16:31:05 +0100

    ADD .env (versionné à des fins pédagogiques)

--- mongo-init/init-users.js
+++ mongo-init/init-users.js
@@ -1,8 +1,8 @@
 db = db.getSiblingDB('datasolutech');
 
 db.createUser({
-  user: "user",
-  pwd: "user",
+  user: process.env.MONGO_USER,
+  pwd: process.env.MONGO_USER_PASSWORD,
   roles: [
     { role: "read", db: "datasolutech" }
   ]
